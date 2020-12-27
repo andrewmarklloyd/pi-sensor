175e927278f2979da8fcf156a4dece9417c44abf
Andrew Lloyd <andrew85.lloyd@gmail.com>
2020-12-26 19:48:55 -0800

    Move websocket to app to account for disconnects

--- consumer/frontend/src/App.js
+++ consumer/frontend/src/App.js
@@ -1,14 +1,15 @@
-import { setupWebSocket, subscribeToChange } from './api';
+// import { setupWebSocket, subscribeToMessages, subscribeToConnect, subscribeToDisconnect } from './websocket';
 import React, { Component } from 'react';
 import './App.css';
+
+var ws
 
 class App extends Component {
   constructor(props) {
     super(props);
-    setupWebSocket();
-    subscribeToChange((data) => this.setState({
-      data
-    }));
+    
+    var app = this
+    setupSockets(app)
   }
 
   state = {
@@ -23,6 +24,37 @@ class App extends Component {
         </p>
       </div>
     );
+  }
+}
+
+function setupSockets(app) {
+  if (window.location.protocol === "https:") {
+    ws = new WebSocket(`wss://${window.location.origin}/ws`);
+  } else {
+    ws = new WebSocket(`ws://localhost:8080/ws`);
+  }
+
+  ws.onerror = function(evt) {
+    console.log("Websocket error: " + evt.data);
+  }
+
+  ws.onmessage = function(evt) {
+    var data = JSON.parse(evt.data)
+    console.log(data.state, data.source)
+    app.setState({
+      data
+    })
+  }
+  
+  ws.onopen = function(evt) {
+    console.log("Connected")
+  }
+  
+  ws.onclose = function(){
+    setTimeout(() => {
+      setupSockets(app)
+    }, 5000)
+    console.log("Disconnected, attempting reconnect in 5 seconds.")
   }
 }
 
