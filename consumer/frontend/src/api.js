d909e9b62050e141e148619589c0b807efa4f7cb
Andrew Lloyd <andrew85.lloyd@gmail.com>
2020-12-26 17:35:39 -0800

    Comment cleanup

--- consumer/frontend/src/api.js
+++ consumer/frontend/src/api.js
@@ -4,17 +4,8 @@ if (window.location.protocol === "https:") {
 } else {
   ws = new WebSocket(`ws://localhost:8080/ws`);
 }
-// var ws = new WebSocket(`ws://${window.location.host}/ws`);
 
 function setupWebSocket(){
-  console.log("setting up websocket")
-  // if (location.protocol == "https:") {
-  //   ws = new WebSocket(`wss://${location.host}/ws`);
-  // } else {
-  //   ws = new WebSocket(`ws://${location.host}/ws`);
-  // }
-
-  // var ws = new WebSocket(`ws://localhost:8080/ws`);
   ws.onclose = function(){
     console.log("Closed connection")
     // setTimeout(setupWebSocket, 1000);
@@ -23,11 +14,6 @@ function setupWebSocket(){
   ws.onopen = function(evt) {
     console.log("Opened connection")
   }
-
-  // ws.onmessage = function(evt) {
-  //   var data = JSON.parse(evt.data)
-  //   console.log(data.state, data.source)
-  // }
 
   ws.onerror = function(evt) {
     console.log("Websocket error: " + evt.data);
