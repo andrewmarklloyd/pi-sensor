b47eeb0ec4a8b3f3aad1e10bbc2b991c4f530b57
Andrew Lloyd <andrew85.lloyd@gmail.com>
2021-05-04 22:21:49 -0700

    Fix caching

--- server/main.go
+++ server/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func init() {
@@ -13,16 +15,19 @@ func init() {
 var (
 	brokerurl = flag.String("brokerurl", os.Getenv("CLOUDMQTT_URL"), "The broker to connect to")
 	topic     = flag.String("topic", os.Getenv("TOPIC"), "The topic to subscribe")
+	redisurl  = flag.String("redisurl", os.Getenv("REDIS_URL"), "The redis cluster to connect to")
 	mockMode  = flag.String("mockMode", os.Getenv("MOCK_MODE"), "Mock mode for local development")
 	logger    = log.New(os.Stdout, "[Pi-Sensor Server] ", log.LstdFlags)
 )
 
 var _webServer webServer
+var _redisClient redisClient
 
 func newClientHandler() {
-	logger.Println("New client handler, sending status for all")
-	// get latest status for all sensors and send them to the client
-	_webServer.sendMessage("garage|OPEN")
+	state, _ := _redisClient.ReadAllState()
+	for k, v := range state {
+		_webServer.sendMessage(fmt.Sprintf("%s|%s", k, v))
+	}
 }
 
 func main() {
@@ -35,6 +40,9 @@ func main() {
 	if *topic == "" {
 		log.Fatalln("topic to publish to is required")
 	}
+	if *redisurl == "" {
+		log.Fatalln("redisurl is required")
+	}
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatalln("PORT must be set")
@@ -44,7 +52,12 @@ func main() {
 
 	mqttClient := newMQTTClient(*brokerurl, *topic)
 	mqttClient.Subscribe(func(message string) {
+		messageSplit := strings.Split(message, "|")
+		_redisClient.WriteState(messageSplit[0], messageSplit[1])
 		_webServer.sendMessage(message)
 	})
+	// var err error
+	_redisClient, _ = newRedisClient(*redisurl)
+
 	_webServer.startServer()
 }
