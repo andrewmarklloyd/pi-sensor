7870d2ba6c2324caae2180ed31f904f080d2b426
Andrew Lloyd <andrew85.lloyd@gmail.com>
2021-09-26 20:18:09 -0700

    Don't output balance

--- scripts/balance-check.sh
+++ scripts/balance-check.sh
@@ -2,7 +2,6 @@
 
 
 balance=$(curl -s https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Balance.json -u "${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}" | /app/jq -r .balance)
-echo "Current balance: $balance"
 limit=0.5
 if (( $(echo "$balance $limit" | awk '{print ($1 < $2)}') )); then
   echo "Twilio balance almost depleted"
