253fff976e13f8c3f4da7c98044605d5e03b47d6
Andrew Lloyd <andrew85.lloyd@gmail.com>
2021-09-26 19:49:27 -0700

    Add script to check twilio account balance

---
+++ scripts/balance-check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+balance=$(curl -s https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Balance.json -u "${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}" | /app/jq -r .balance)
+echo "Current balance: $balance"
+limit=13.93
+if (( $(echo "$balance $limit" | awk '{print ($1 < $2)}') )); then
+  echo "Twilio balance almost depleted"
+fi
