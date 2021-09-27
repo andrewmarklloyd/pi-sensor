07e3bbf0240f7403d13dec344083b74fa987325d
Andrew Lloyd <andrew85.lloyd@gmail.com>
2021-09-26 20:00:21 -0700

    Set lower limit for twilio balance

--- scripts/balance-check.sh
+++ scripts/balance-check.sh
@@ -3,7 +3,7 @@
 
 balance=$(curl -s https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Balance.json -u "${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}" | /app/jq -r .balance)
 echo "Current balance: $balance"
-limit=13.93
+limit=0.5
 if (( $(echo "$balance $limit" | awk '{print ($1 < $2)}') )); then
   echo "Twilio balance almost depleted"
 fi
