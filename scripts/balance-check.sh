97562fe5d77d69d73f93f9af86b66b4b5aa1d201
Andrew Lloyd <andrew85.lloyd@gmail.com>
2021-09-28 19:37:20 -0700

    Output balance for tracibility

--- scripts/balance-check.sh
+++ scripts/balance-check.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 
-balance=$(curl -s https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Balance.json -u "${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}" | /app/jq -r .balance)
+balance=$(curl -s https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Balance.json -u "${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}" | jq -r .balance)
+echo "Current balance: ${balance}"
 limit=0.5
-if (( $(echo "$balance $limit" | awk '{print ($1 < $2)}') )); then
+if (( $(echo "${balance} ${limit}" | awk '{print ($1 < $2)}') )); then
   echo "Twilio balance almost depleted"
 fi
