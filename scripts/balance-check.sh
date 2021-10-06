4114fe21b943ffd77f78a6939e154baa091ae6a7
Andrew Lloyd <andrew85.lloyd@gmail.com>
2021-10-06 09:24:57 -0700

    Use jq in app

--- scripts/balance-check.sh
+++ scripts/balance-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-balance=$(curl -s https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Balance.json -u "${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}" | jq -r .balance)
+balance=$(curl -s https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Balance.json -u "${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}" | /app/jq -r .balance)
 echo "Current balance: ${balance}"
 limit=0.5
 if (( $(echo "${balance} ${limit}" | awk '{print ($1 < $2)}') )); then
