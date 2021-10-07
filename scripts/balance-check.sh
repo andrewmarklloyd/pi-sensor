405370917451956c8896d0c54e660aee66251c9c
Andrew Lloyd <andrew85.lloyd@gmail.com>
2021-10-06 20:49:25 -0700

    Allow mac and linux to run balance check

--- scripts/balance-check.sh
+++ scripts/balance-check.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+set -euo pipefail
 
-balance=$(curl -s https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Balance.json -u "${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}" | /app/jq -r .balance)
+json=$(curl -s https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Balance.json -u "${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}")
+
+if [[ $(uname) == "Darwin" ]]; then
+  jq=$(which jq)
+else
+  jq='/app/jq'
+fi
+
+balance=$(echo ${json} | ${jq} -r .balance)
 echo "Current balance: ${balance}"
 limit=0.5
 if (( $(echo "${balance} ${limit}" | awk '{print ($1 < $2)}') )); then
