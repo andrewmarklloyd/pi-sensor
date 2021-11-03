11909e5e8d1ee30bea350784245af8cb74a8d835
Andrew Lloyd <andrew85.lloyd@gmail.com>
2021-11-02 21:11:14 -0700

    Update limit rows script

--- scripts/limit-rows.sh
+++ scripts/limit-rows.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
 
 
-limit_data_size() {
-    # copy file from object storage, output to tmp/storage.csv
-    latest=$(tail -n 1 tmp/storage.csv || echo '')
-    if [[ -z ${latest} ]]; then
-        limitTs='1609491600' # 2021-01-01
-    else
-        arrIN=(${latest//,/ })
-        limitTs=${arrIN[2]}
-    fi
+get_all() {
     export DATABASE_URL=$(heroku config:get DATABASE_URL -a pi-sensor-staging)
     eval `~/parse-posgres-url.js`
-    limit=100
-    query="\copy (SELECT * FROM status WHERE timestamp > '${limitTs}' ORDER by timestamp ASC LIMIT ${limit}) to '/tmp/out.csv' with delimiter as ','"
-    docker run -v "${PWD}/tmp:/tmp" -e PGPASSWORD=${pw} -it --rm postgres psql -h ${host} -U ${user} ${db} -c "${query}"
-    cat tmp/out.csv >> tmp/storage.csv
-    # copy tmp/storage.csv to object storage
+    query="\copy (SELECT * FROM status ORDER by timestamp ASC) to '/tmp/backup-full.csv' with delimiter as ','"
+    docker run -v "${PWD}/tmp:/tmp" -e PGPASSWORD=${pw} -it --rm postgres psql -h ${host} -U ${user} ${db} -t -c "${query}"
+}
+
+limit_data_size() {
+    export DATABASE_URL=$(heroku config:get DATABASE_URL -a pi-sensor-staging)
+    eval `~/parse-posgres-url.js`
+    max=400
+    query="SELECT COUNT(*) FROM status"
+    rowCount=$(docker run -v "${PWD}/tmp:/tmp" -e PGPASSWORD=${pw} -it --rm postgres psql -h ${host} -U ${user} ${db} -t -c "${query}")
+    rowCount=$(echo "$rowCount" | tr -d '[:space:]')
+    if [[ ${rowCount} -le ${max} ]]; then
+        echo "Row count: '${rowCount}' less than or equal to max: '${max}', no action required"
+        return 0
+    fi
+    rowsAboveMax=$((${rowCount}-${max}))
+    echo "Number of rows above max: ${rowsAboveMax}"
+    export DATABASE_URL=$(heroku config:get DATABASE_URL -a pi-sensor-staging)
+    eval `~/parse-posgres-url.js`
+    query="\copy (SELECT * FROM status ORDER by timestamp ASC LIMIT ${rowsAboveMax}) to '/tmp/out.csv' with delimiter as ','"
+    docker run -v "${PWD}/tmp:/tmp" -e PGPASSWORD=${pw} -it --rm postgres psql -h ${host} -U ${user} ${db} -t -c "${query}"
+    # TODO: copy file from object storage, output to tmp/backup-full.csv
+    if [[ -f ~/Desktop/backup-full.csv ]]; then
+        cp ~/Desktop/backup-full.csv tmp/backup-full.csv
+    fi
+    latest=$(tail -n 1 tmp/backup-full.csv || echo '')
+    if [[ -z ${latest} ]]; then
+        echo "Backup file not found, dumping contents of query to tmp/out.csv to backup file"
+        cp tmp/out.csv tmp/backup-full.csv
+    else
+        awk "/${latest}/{y=1;next}y" tmp/out.csv >> tmp/backup-full.csv
+    fi
+    # TODO: copy tmp/backup-full.csv to object storage
+    cp tmp/backup-full.csv ~/Desktop/
 }
 
 limit_data_size
