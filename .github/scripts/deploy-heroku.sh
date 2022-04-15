4f8e033cec172e982134c50c0d9573ea360e0c11
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-04-14 23:08:59 -0700

    Fix

--- .github/scripts/deploy-heroku.sh
+++ .github/scripts/deploy-heroku.sh
@@ -19,9 +19,12 @@ heroku container:login
 heroku container:push web -a ${app}
 heroku container:release web -a ${app}
 
+echo "Deploying version ${GITHUB_SHA}"
+
 version="unknown"
 i=0
 while [[ ${version} != ${GITHUB_SHA} ]]; do
+  echo "Attempt number ${i}, deployed version: ${version}"
   if [[ ${i} -gt 12 ]]; then
     echo "Exceeded max attempts checking deployment health, deployment failed"
     exit 1
