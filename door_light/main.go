8f775b03619b19bc28f473b91f12026d847a5ac2
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-06-17 17:30:41 -0700

    Use zap logger for door-light app

--- door_light/main.go
+++ door_light/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 	"time"
 
 	mqttC "github.com/eclipse/paho.mqtt.golang"
+	"go.uber.org/zap"
 
 	"github.com/andrewmarklloyd/pi-sensor/internal/pkg/config"
 	"github.com/andrewmarklloyd/pi-sensor/internal/pkg/mqtt"
@@ -17,7 +17,7 @@ import (
 )
 
 var (
-	logger = log.New(os.Stdout, "[Pi-Sensor Outlet] ", log.LstdFlags)
+	logger *zap.SugaredLogger
 )
 
 const (
@@ -26,11 +26,18 @@ const (
 )
 
 func main() {
+	l, _ := zap.NewProduction()
+
+	logger = l.Sugar().Named("pi-sensor-door-light")
+	defer logger.Sync()
+
 	appVersion := os.Getenv("APP_VERSION")
 	if appVersion == "" {
 		appVersion = "Unknown"
 	}
-	logger.Println(fmt.Sprintf("Running app version: %s", appVersion))
+
+	logger.Infof("Running app version: %s", appVersion)
+
 	brokerurl := flag.String("brokerurl", os.Getenv("CLOUDMQTT_URL"), "The broker to connect to")
 	appUser := flag.String("appUser", os.Getenv("CLOUDMQTT_APP_USER"), "The MQTT app user to connect")
 	appUserPassword := flag.String("appUserPassword", os.Getenv("CLOUDMQTT_APP_PASSWORD"), "The MQTT app user password to connect")
@@ -38,20 +45,20 @@ func main() {
 	door := flag.String("door", os.Getenv("DOOR_LIGHT_DOOR"), "The door to monitor")
 
 	if *brokerurl == "" {
-		logger.Fatalln("at least one broker is required")
+		logger.Fatal("at least one broker is required")
 	}
 
 	if *appUser == "" {
-		logger.Fatalln("appUser is required")
+		logger.Fatal("appUser is required")
 	}
 
 	if *appUserPassword == "" {
-		logger.Fatalln("appUserPassword is required")
+		logger.Fatal("appUserPassword is required")
 	}
 
 	urlSplit := strings.Split(*brokerurl, "@")
 	if len(urlSplit) != 2 {
-		logger.Fatalln("unexpected CLOUDMQTT_URL parsing error")
+		logger.Fatal("unexpected CLOUDMQTT_URL parsing error")
 	}
 	domain := urlSplit[1]
 
@@ -59,23 +66,23 @@ func main() {
 
 	deviceNames := strings.Split(*deviceNamesArg, ",")
 	if len(deviceNames) == 0 {
-		logger.Fatalln("devicenames required")
+		logger.Fatal("devicenames required")
 	}
 
 	if *door == "" {
-		logger.Fatalln("door required")
+		logger.Fatal("door required")
 	}
 
 	allDevices, err := hs100.Discover("192.168.1.1/24", configuration.Default().WithTimeout(time.Second))
 	if err != nil {
-		logger.Fatalln(fmt.Errorf("Error getting devices: %s", err))
+		logger.Fatalf("Error getting devices: %s", err)
 	}
 
 	var targetDevices []hs100.Hs100
 	for _, d := range allDevices {
 		name, err := d.GetName()
 		if err != nil {
-			logger.Fatalln(fmt.Errorf("Error getting device name: %s", err))
+			logger.Fatalf("Error getting device name: %s", err)
 		}
 		for _, n := range deviceNames {
 			if name == n {
@@ -85,7 +92,7 @@ func main() {
 	}
 
 	if len(targetDevices) == 0 {
-		logger.Fatalln(fmt.Sprintf("None of discovered devices matches expected device names: %s", deviceNames))
+		logger.Fatalf("None of discovered devices matches expected device names: %s", deviceNames)
 	}
 
 	h := config.Heartbeat{
@@ -95,13 +102,13 @@ func main() {
 	}
 
 	mqttClient := mqtt.NewMQTTClient(mqttAddr, func(client mqttC.Client) {
-		logger.Println("Connected to MQTT server")
+		logger.Info("Connected to MQTT server")
 	}, func(client mqttC.Client, err error) {
 		logger.Fatalf("Connection to MQTT server lost: %v", err)
 	})
 	err = mqttClient.Connect()
 	if err != nil {
-		logger.Fatalln("error connecting to mqtt:", err)
+		logger.Fatalf("error connecting to mqtt: %s", err)
 	}
 
 	ticker := time.NewTicker(heartbeatIntervalSeconds * time.Second)
@@ -109,7 +116,7 @@ func main() {
 		for range ticker.C {
 			err := mqttClient.PublishHeartbeat(h)
 			if err != nil {
-				logger.Println("error publishing heartbeat:", err)
+				logger.Errorf("error publishing heartbeat: %s", err)
 			}
 		}
 	}()
@@ -118,7 +125,7 @@ func main() {
 		for _, d := range targetDevices {
 			err := triggerOutlet(d, messageString, *door)
 			if err != nil {
-				logger.Println(fmt.Errorf("Error triggering outlet: %s", err))
+				logger.Errorf("Error triggering outlet: %s", err)
 			}
 		}
 	})
@@ -142,10 +149,10 @@ func triggerOutlet(outlet hs100.Hs100, messageString string, door string) error 
 		return fmt.Errorf("getting outlet name: %s", err)
 	}
 	if strings.Contains(messageString, config.OPEN) {
-		logger.Println(fmt.Sprintf("Turning %s outlet ON", name))
+		logger.Infof("Turning %s outlet ON", name)
 		return outlet.TurnOn()
 	} else if strings.Contains(messageString, config.CLOSED) {
-		logger.Println(fmt.Sprintf("Turning %s outlet OFF", name))
+		logger.Infof("Turning %s outlet OFF", name)
 		return outlet.TurnOff()
 	}
 	return fmt.Errorf(fmt.Sprintf("Message did not contain %s or %s", config.OPEN, config.CLOSED))
