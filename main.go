bc27af2e3d12feacefeb4b764470805c99568f5b
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-09-18 17:23:43 -0700

    Re add main cmd

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"github.com/andrewmarklloyd/pi-sensor/cmd"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("ca66c829-a2e1-4ecb-abf9-5f6e03dabd97")
-		fmt.Fprintf(w, "Hello")
-	})
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	cmd.Execute()
 }
