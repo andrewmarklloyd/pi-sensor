59bd8eb6c402445414f5249eebc07489552ea035
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-10-17 20:45:49 -0700

    fix version

--- .github/scripts/deploy-do.sh
+++ .github/scripts/deploy-do.sh
@@ -10,7 +10,7 @@ fi
 
 if ! command -v doctl &> /dev/null; then
   wget -q https://github.com/digitalocean/doctl/releases/download/v1.83.0/doctl-1.83.0-linux-amd64.tar.gz -P /tmp
-  tar xf /tmp/doctl-1.79.0-linux-amd64.tar.gz -C /tmp
+  tar xf /tmp/doctl-1.83.0-linux-amd64.tar.gz -C /tmp
   mv /tmp/doctl /usr/local/bin
 fi
 
