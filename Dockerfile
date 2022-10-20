cc4657d640e0dd6737c1562bb7618192b55735ab
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-10-19 17:35:47 -0700

    Use firewall entrypoint tool

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
+FROM golang:1.19-alpine as builder
+RUN go install github.com/andrewmarklloyd/do-app-firewall-entrypoint@latest
+
 FROM alpine
-
-RUN apk add curl jq
-
+COPY --from=builder /go/bin/do-app-firewall-entrypoint /app/do-app-firewall-entrypoint
 COPY build/pi-sensor-server /app/
 COPY frontend/build /app/frontend/build
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,42 +1,8 @@
 #!/bin/sh
 
 
-list_firewalls() {
-    curl -s -X GET \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Bearer $DO_ACCESS_TOKEN" \
-        "https://api.digitalocean.com/v2/firewalls"
-}
-
-get_firewall() {
-    id=${1}
-    curl -s -X GET \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Bearer $DO_ACCESS_TOKEN" \
-        "https://api.digitalocean.com/v2/firewalls/${id}"
-}
-
-configure() {
-    IP=$(curl -s ifconfig.me)
-    firewalls=$(list_firewalls)
-    id=$(echo ${firewalls} | jq -r ".firewalls[] | select(.name == \"${FIREWALL_NAME}\").id")
-    firewall=$(get_firewall ${id})
-
-    existing=$(echo ${firewall} | jq --arg ip ${IP} '.firewall.inbound_rules[] | select(.ports == "8080")')
-    tmp=$(echo ${existing} | jq "del(.sources.addresses[] | select(. != \"${STATIC_INBOUND_IP}\"))")
-    updatedRules=$(echo $tmp | jq ".sources.addresses += [\"${IP}\"]" | jq -s)
-    newFirewall=$(echo ${firewall} | jq --arg name "${FIREWALL_NAME}" --argjson rules "${updatedRules}" '.firewall.inbound_rules=$rules | .firewall')
-    
-    curl -s -X PUT \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Bearer ${DO_ACCESS_TOKEN}" \
-        -d "${newFirewall}" \
-        "https://api.digitalocean.com/v2/firewalls/${id}" >/dev/null
-}
-
-
 if [ ${RUNTIME} == "D_O" ]; then
-    configure
+    /app/do-app-firewall-entrypoint
 fi
 
 unset DO_TOKEN
