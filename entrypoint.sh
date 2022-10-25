27fc2ac6a84598b9a33d6ec9a68a160d904ef672
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-10-25 12:07:28 -0700

    Use DO token from onepassword

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 
 if [ ${RUNTIME} == "D_O" ]; then
+    export DO_ACCESS_TOKEN=$(op read op://pi-sensor-server/config/DO_TOKEN)
     /app/do-app-firewall-entrypoint
+    unset DO_ACCESS_TOKEN
+    unset DO_FIREWALL_ID
 fi
-
-unset DO_TOKEN
-unset DO_FIREWALL_ID
 
 /app/op run --env-file="/app/.env.server.tmpl" -- /app/pi-sensor-server
