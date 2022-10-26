002ebf9b3da9cd3851925e3e0fe17a7be01fab4d
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-10-25 21:32:56 -0700

    debug

--- .github/scripts/deploy-agent.sh
+++ .github/scripts/deploy-agent.sh
@@ -3,11 +3,12 @@
 
 set -euo pipefail
 
-# eval `ssh-agent -s`
+curl -sSfo op.zip https://cache.agilebits.com/dist/1P/op2/pkg/v2.7.1/op_linux_amd64_v2.7.1.zip
+unzip -od /usr/local/bin/ op.zip
+rm op.zip
 
 mkdir -p ~/.ssh/
-echo "${AGENT_SSH_KEY}" > ~/.ssh/id
+/usr/local/bin/op read op://github-ci/pi-sensor-agent-ssh-key/private\ key > ~/.ssh/id
 chmod 600 ~/.ssh/id
-# ssh-add ~/.ssh/id
 ssh -i ~/.ssh/id pi@${AGENT_HOST} exit
 echo $?
