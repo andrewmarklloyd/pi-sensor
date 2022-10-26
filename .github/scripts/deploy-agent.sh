1a6afa768b8eba85a419509a69c46e86916160a9
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-10-25 21:10:38 -0700

    debug

--- .github/scripts/deploy-agent.sh
+++ .github/scripts/deploy-agent.sh
@@ -3,8 +3,10 @@
 
 set -euo pipefail
 
-eval `ssh-agent -s`
+# eval `ssh-agent -s`
 
-echo "$AGENT_SSH_KEY" | ssh-add -
-# echo "${AGENT_SSH_KEY}" > key
-# ssh-add key
+echo "${AGENT_SSH_KEY}" > ~/.ssh/id
+chmod 600 ~/.ssh/id
+# ssh-add ~/.ssh/id
+ssh -i ~/.ssh/id pi@${AGENT_HOST} exit
+echo $?
