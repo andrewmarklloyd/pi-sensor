bee3cef4f204e636d31a587e25866189089882c4
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-10-26 21:14:04 -0700

    fix

--- .github/scripts/deploy-agent.sh
+++ .github/scripts/deploy-agent.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-
 set -euo pipefail
 
-curl -sSfo op.zip https://cache.agilebits.com/dist/1P/op2/pkg/v2.7.1/op_linux_amd64_v2.7.1.zip
-unzip -od /usr/local/bin/ op.zip
-rm op.zip
-
-/usr/local/bin/op document get pi-sensor-agent-ssh-key-id_ed25519 > id_ed25519
-chmod 600 id_ed25519
-ssh -o StrictHostKeyChecking=no -i id_ed25519 pi@${AGENT_HOST} uptime
-rm id_ed25519
+ssh-add - <<< "${AGENT_SSH_KEY}"
+ssh -o StrictHostKeyChecking=no pi@${AGENT_HOST} uptime
