09f88a55493250c190f1d5549009a2659cab16a6
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-10-29 21:48:53 -0700

    Remove twilio references

--- internal/pkg/clients/clients.go
+++ internal/pkg/clients/clients.go
@@ -3,15 +3,13 @@ package clients
 import (
 	"github.com/andrewmarklloyd/pi-sensor/internal/pkg/aws"
 	"github.com/andrewmarklloyd/pi-sensor/internal/pkg/mqtt"
-	"github.com/andrewmarklloyd/pi-sensor/internal/pkg/notification"
 	"github.com/andrewmarklloyd/pi-sensor/internal/pkg/postgres"
 	"github.com/andrewmarklloyd/pi-sensor/internal/pkg/redis"
 )
 
 type ServerClients struct {
-	Redis     redis.Client
-	Postgres  postgres.Client
-	Messenger notification.Messenger
-	Mqtt      mqtt.MqttClient
-	AWS       aws.Client
+	Redis    redis.Client
+	Postgres postgres.Client
+	Mqtt     mqtt.MqttClient
+	AWS      aws.Client
 }
