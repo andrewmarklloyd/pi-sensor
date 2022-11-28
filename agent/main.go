0d31a9d08cea8b14b2fee8549672b542ab86273b
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-11-27 21:45:41 -0800

    debug

--- agent/main.go
+++ agent/main.go
@@ -92,7 +92,7 @@ func main() {
 	}
 	pinClient := gpio.NewPinClient(pinNum, mockMode)
 
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
--- test/test-data/main.go
+++ test/test-data/main.go
@@ -34,7 +34,7 @@ func main() {
 
 	mqttClient := newMQTTClient(*brokerurl, topic, *sensorSource)
 
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
