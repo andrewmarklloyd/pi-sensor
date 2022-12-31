77ebbff351f05cf85e974bd99c3f10e8028de6fd
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-12-31 15:37:57 -0800

    Don't install nvm/npm; already done in github actions

--- .github/scripts/build-front.sh
+++ .github/scripts/build-front.sh
@@ -2,13 +2,5 @@
 
 set -euo pipefail
 
-if ! command -v nvm &> /dev/null; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-    export NVM_DIR="${HOME}/.nvm"
-    source "${NVM_DIR}/nvm.sh"
-    cd frontend
-    nvm install
-    nvm use
-fi
 npm install
 npm run build
