c3654dc7874429124783f789d490d966b102e03e
Andrew Lloyd <andrew85.lloyd@gmail.com>
2023-01-19 22:13:30 -0800

    Log token remaining days

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,7 @@ fi
 unset DO_ACCESS_TOKEN
 unset DO_FIREWALL_ID
 
+exp=$(op read op://pi-sensor-server/config/OP_TOKEN_EXP)
+remaining=$(( ($(date -d ${exp} +%s) - $(date +%s) )/(60*60*24) ))
+echo "Token remaining days: ${remaining}"
 /app/op run --env-file="/app/.env.server.tmpl" -- /app/pi-sensor-server
