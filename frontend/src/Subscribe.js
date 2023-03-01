198be9869c0c7ae3be19afa3d6ce7b56223904e3
Andrew Lloyd <andrew85.lloyd@gmail.com>
2023-02-28 21:10:34 -0800

    Remove useless constructor

--- frontend/src/Subscribe.js
+++ frontend/src/Subscribe.js
@@ -7,15 +7,6 @@ import {
 import SiteWrapper from "./SiteWrapper";
 
 class SubscribePage extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  componentDidMount() {
-    
-  }
-
-  
   render() {
     return (
       <SiteWrapper>
