1cc6ec5ccf6a89cdee6231577ade39e0d38c2d8c
Andrew Lloyd <andrew85.lloyd@gmail.com>
2023-03-13 20:36:59 -0700

    Use uuid for local dev key

--- local-dev.sh
+++ local-dev.sh
@@ -9,7 +9,8 @@ export CLOUDMQTT_URL=mqtt://user:pass@127.0.0.1:1883
 export CLOUDMQTT_SERVER_PASSWORD=abc
 export CLOUDMQTT_SERVER_USER=abc
 export PORT=8080
-
+# random uuid
+export ENCRYPTION_KEY=074d3351-4c59-434a-ba52-4bedf972
 export GOOGLE_CLIENT_ID=$(op read op://pi-sensor-server/config/GOOGLE_CLIENT_ID)
 export GOOGLE_CLIENT_SECRET=$(op read op://pi-sensor-server/config/GOOGLE_CLIENT_SECRET)
 export REDIRECT_URL=http://localhost:8080/google/callback
