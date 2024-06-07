958d2ce76aa489d665771aae05288aaffae4a6a3
Andrew Lloyd <andrew85.lloyd@gmail.com>
2024-06-06 20:17:09 -0700

    Use docker push instead of artifacts

--- .github/scripts/deploy-do.sh
+++ .github/scripts/deploy-do.sh
@@ -17,10 +17,6 @@ fi
 
 deploy() {
   echo "Deploying version ${SHORT_SHA}"
-  echo $GH_TOKEN | docker login ghcr.io -u 'andrewmarklloyd' --password-stdin
-  image="ghcr.io/andrewmarklloyd/pi-sensor:${SHORT_SHA}"
-  docker build -t ${image} .
-  docker push ${image}
 
   DO_APP_ID=$(doctl --access-token ${DO_ACCESS_TOKEN} apps list -o json | yq -r '.[] | select(.spec.name == "pi-sensor").id')
 
@@ -31,26 +27,6 @@ deploy() {
   rm -f ${TFILE}
 }
 
-cleanup_tags() {
-  maxTags=5
-  tags=$(doctl --access-token ${DO_ACCESS_TOKEN} registry repo list-tags pi-sensor -o json)
-  num=$(echo ${tags} | jq 'length')
-  if [ "${num}" -gt "${maxTags}" ]; then
-    diff=$((${num}-${maxTags}))
-    echo "deleting oldest ${diff} tags from the container registry"
-    toDelete=$(echo "${tags}" | jq -r '.[].tag' | tail -${diff})
-    doctl --access-token ${DO_ACCESS_TOKEN} registry repo delete-tag pi-sensor --force ${toDelete}
-  fi
-
-  untaggedManifests=$(doctl --access-token ${DO_ACCESS_TOKEN} registry repo list-manifests pi-sensor -o json | jq -r '.[] | select(.tags | length==0) | .digest')
-  if [[ ! -z ${untaggedManifests} ]]; then
-    num=$(echo "${untaggedManifests}" | wc -l)
-    echo "deleting ${num} untagged manifests from the container registry"
-    doctl --access-token ${DO_ACCESS_TOKEN} registry repo delete-manifest pi-sensor ${untaggedManifests} --force
-  fi
-}
-
 git diff ':!frontend/package-lock.json' ':!frontend/public/index.html' ':!frontend/public/*service-worker*'
 SHORT_SHA=$(echo ${GITHUB_SHA} | cut -c1-7)
 deploy
-cleanup_tags
