ed859eab56d0f49dbec57c8e439e99f60bf05725
Andrew Lloyd <andrew85.lloyd@gmail.com>
2025-01-31 22:02:13 -0800

    Remove unused func

--- internal/pkg/datadog/datadog.go
+++ internal/pkg/datadog/datadog.go
@@ -80,58 +80,6 @@ func (c *Client) PublishTokenDaysLeft(ctx context.Context, tokenMetadata config.
 	return nil
 }
 
-func (c *Client) PublishCertDaysLeft(ctx context.Context, tokenMetadata config.TokenMetadata) error {
-	expTime, err := time.Parse(time.RFC3339, tokenMetadata.Expiration)
-	if err != nil {
-		return fmt.Errorf("parsing time from token expiration: %w", err)
-	}
-
-	valueCtx := context.WithValue(
-		ctx,
-		datadog.ContextAPIKeys,
-		map[string]datadog.APIKey{
-			"apiKeyAuth": {
-				Key: c.apiKey,
-			},
-			"appKeyAuth": {
-				Key: c.appKey,
-			},
-		},
-	)
-
-	body := datadogV2.MetricPayload{
-		Series: []datadogV2.MetricSeries{
-			{
-				Metric: "token.days_left",
-				Type:   datadogV2.METRICINTAKETYPE_COUNT.Ptr(),
-				Points: []datadogV2.MetricPoint{
-					{
-						Timestamp: datadog.PtrInt64(time.Now().Unix()),
-						Value:     datadog.PtrFloat64(time.Until(expTime).Hours() / 24),
-					},
-				},
-				Resources: []datadogV2.MetricResource{
-					{
-						Type: datadog.PtrString("tokenname"),
-						Name: datadog.PtrString(tokenMetadata.Name),
-					},
-					{
-						Type: datadog.PtrString("owner"),
-						Name: datadog.PtrString(tokenMetadata.Owner),
-					},
-				},
-			},
-		},
-	}
-
-	_, _, err = c.api.SubmitMetrics(valueCtx, body, *datadogV2.NewSubmitMetricsOptionalParameters())
-	if err != nil {
-		return fmt.Errorf("submitting metrics: %s", err)
-	}
-
-	return nil
-}
-
 func (c *Client) PublishHeartbeat(ctx context.Context, agent string) error {
 	valueCtx := context.WithValue(
 		ctx,
