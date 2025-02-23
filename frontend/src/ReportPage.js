d6a659710b72cf41c9a39ba532a0f7948379fd69
Andrew Lloyd <andrew85.lloyd@gmail.com>
2025-02-23 00:00:52 -0800

    Start report page migration

--- frontend/src/ReportPage.js
+++ frontend/src/ReportPage.js
@@ -1,18 +1,48 @@
 import React, { Component } from "react";
+import { DataGrid } from '@mui/x-data-grid';
+import Card from '@mui/material/Card';
+import FormControl from '@mui/material/FormControl';
 
 import {
   Page,
-  Card,
+  Card as CardTabler,
   Form,
   Table
 } from "tabler-react";
 
 import { trimVersion, unixToDate } from "./DataModel";
 
+const columns = [
+  {
+    field: 'timestamp',
+    renderHeader: () => (<strong>{'Time'}</strong>),
+    // width: 110,
+    editable: false,
+  },
+  {
+    field: 'source',
+    renderHeader: () => (<strong>{'Door'}</strong>),
+    // width: 210,
+    editable: false,
+  },
+  {
+    field: 'status',
+    renderHeader: () => (<strong>{'Status'}</strong>),
+    // width: 150,
+    editable: false
+  },
+  {
+    field: 'version',
+    renderHeader: () => (<strong>{'Version'}</strong>),
+    // width: 100,
+    editable: false
+  }
+]
+
 class ReportPage extends Component {
   constructor(props) {
     super(props)
-    this.state = {messages: [], sensors: ['', 'All'], numPages: 1, page: 1}
+    this.state = {rows: [], messages: [], sensors: ['', 'All'], numPages: 1, page: 1}
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -62,7 +92,16 @@ class ReportPage extends Component {
     })
     .then(r => r.json())
     .then(res => {
-      component.setState({messages: res.messages, numPages: res.numPages})
+      let messages = []
+      res.messages.map(item => {
+        messages.push({
+          timestamp: unixToDate(item.timestamp),
+          source: item.source,
+          status: item.status,
+          version: trimVersion(item.version)
+        })
+      })
+      component.setState({rows: messages, messages: messages, numPages: res.numPages})
     })
   }
 
@@ -80,46 +119,95 @@ class ReportPage extends Component {
   
   render() {
     return (
-      <Page.Content>
-      <Card>
-        <Card.Header>
-          <Form.Group label="">
-            <h4>Sensor</h4>
-            <Form.Select onChange={this.handleChange}>
-              {this.state.sensors.map((item, index) => (
-                <option key={index} value={item}>
-                  {item}
-                </option>
-                ))}
-            </Form.Select>
-            <Form.Select onChange={this.handleChange}>
-              {this.getPageOptions()}
-            </Form.Select>
-          </Form.Group>
-        </Card.Header>
-        <Card.Body>
-          <Table>
-            <Table.Header>
-              <Table.ColHeader>Time</Table.ColHeader>
-              <Table.ColHeader>Door</Table.ColHeader>
-              <Table.ColHeader>Status</Table.ColHeader>
-              <Table.ColHeader>Version</Table.ColHeader>
-            </Table.Header>
-            <Table.Body>
-            {this.state.messages.map(item => (
-              <Table.Row>
-                <Table.Col>{unixToDate(item.timestamp)}</Table.Col>
-                <Table.Col>{item.source}</Table.Col>
-                <Table.Col>{item.status}</Table.Col>
-                <Table.Col>{trimVersion(item.version)}</Table.Col>
-              </Table.Row>
-            ))
-            }
-            </Table.Body>
-          </Table>
-        </Card.Body>
-      </Card>
-      </Page.Content>
+      <div>
+        <Card>
+        <FormControl fullWidth>
+          
+        </FormControl>
+        </Card>
+        <Form.Group label="">
+          <h4>Sensor</h4>
+          <Form.Select onChange={this.handleChange}>
+            {this.state.sensors.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+              ))}
+          </Form.Select>
+          <Form.Select onChange={this.handleChange}>
+            {this.getPageOptions()}
+          </Form.Select>
+        </Form.Group>
+        <Card sx={{ m: 10}}>
+        <DataGrid
+        sx={{
+          '.MuiDataGrid-columnHeaderTitleContainer': {
+            whiteSpace: 'normal',
+            wordWrap: 'break-word',
+            padding: '5px'
+          },
+          '.MuiDataGrid-cell': {
+              whiteSpace: 'normal',
+              wordWrap: 'break-word',
+              padding:'5px'
+          }
+        }}
+        getRowHeight={() =>{ return 'auto'}}
+        getRowId={(row) => row.timestamp}
+        rows={this.state.rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+              paginationModel: {
+              pageSize: 50,
+              },
+          },
+        }}
+        pageSizeOptions={[25, 50, 100]}
+      />
+        </Card>
+
+        {/* <Page.Content>
+        <CardTabler>
+          <CardTabler.Header>
+            <Form.Group label="">
+              <h4>Sensor</h4>
+              <Form.Select onChange={this.handleChange}>
+                {this.state.sensors.map((item, index) => (
+                  <option key={index} value={item}>
+                    {item}
+                  </option>
+                  ))}
+              </Form.Select>
+              <Form.Select onChange={this.handleChange}>
+                {this.getPageOptions()}
+              </Form.Select>
+            </Form.Group>
+          </CardTabler.Header>
+          <CardTabler.Body>
+            <Table>
+              <Table.Header>
+                <Table.ColHeader>Time</Table.ColHeader>
+                <Table.ColHeader>Door</Table.ColHeader>
+                <Table.ColHeader>Status</Table.ColHeader>
+                <Table.ColHeader>Version</Table.ColHeader>
+              </Table.Header>
+              <Table.Body>
+              {this.state.messages.map(item => (
+                <Table.Row>
+                  <Table.Col>{unixToDate(item.timestamp)}</Table.Col>
+                  <Table.Col>{item.source}</Table.Col>
+                  <Table.Col>{item.status}</Table.Col>
+                  <Table.Col>{trimVersion(item.version)}</Table.Col>
+                </Table.Row>
+              ))
+              }
+              </Table.Body>
+            </Table>
+          </CardTabler.Body>
+        </CardTabler>
+        </Page.Content> */}
+      </div>
     );
   }
 }
